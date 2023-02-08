d6e9b13b79946560668279f3bbe943f6f0e896a8
Arthur Borges <arthur@arpiatecnologia.com.br>
2023-02-08 10:47:18 -0300

    feat: add initial endpoints and configurations

---
+++ Endpoints/DTO/GetIngredientsByNameResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace IRecepiesApp.Endpoints.DTO;
+
+public class GetIngredientsByNameResponseDTO
+{
+    public List<Results> Results { get; set; }
+
+}
+
+public partial class Results
+{
+    public string Search_value { get; set; }
+    public string Type { get; set; }
+    public string Display { get; set; }
+}
